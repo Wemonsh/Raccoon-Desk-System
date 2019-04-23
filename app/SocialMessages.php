65fb4782b8c0e6738a73843828f37661ff2d6acf
Oleg Semenov <wemonsh.gg@gmail.com>
2019-04-23 21:09:53 +0300

    MessagesController - MessageModel

--- app/SocialMessages.php
+++ app/SocialMessages.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialMessages extends Model
 {
-    //
+    protected $table = 'user_messages';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+
+    protected $fillable = ['id','message','files','id_sender','id_receiver',];
+
+    public function sender(){
+        return $this->hasOne('App\User','id','id_sender');
+    }
+
+    public function receiver(){
+        return $this->hasOne('App\User','id','id_receiver');
+    }
 }
