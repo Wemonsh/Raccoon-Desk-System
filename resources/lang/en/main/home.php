27059f895095fea092e292715a23605d95ff0603
anndyachenko <anna.dyachenko.watson@gmail.com>
2019-04-30 18:51:30 +0300

    Локализация Home (MainPage) - [En Ru Ua]
    Создание словарей для Home (MainPage) - английский, русский, украинский языки.
    Правка views\home.blade в соответствии с переводом.

---
+++ resources/lang/en/main/home.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'main' => 'Main',
+    'system_description' => 'Automated information system for processing requests and keeping accounting of the organization’s material and technical resources',
+    'users' => 'users',
+    'more' => 'Details',
+    'articles' => 'articles',
+    'to_knowledge_base' => 'Go to the knowledge base',
+    'applications_to_support' => 'application (s) to technical support',
+    'contact_support' => 'Contact the Support',
+    'upcoming_month_events' => 'upcoming events this month',
+    'user_requests_comparison' => 'Analytical comparison of users requests',
+    'updated' => 'Updated',
+    'applications_percentage' => 'Percentage of applications',
+    'news' => 'News',
+    'image' => 'Image',
+    'no_news' => 'There is no news in this category',
+    // TODO Сделать множетвенное число
+    'articles_2' => 'articles',
+    'no_articles' => 'There are no articles in this category',
+
+
+];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-<h1>Главная</h1>
+<h1>{{ __('main/home.main') }}</h1>
 <hr>
-<p>Автоматизированная информационная система обработки обращений и ведения учета материально технических средств организации</p>
+<p>{{ __('main/home.system_description') }}</p>
 
 <div class="row">
     <div class="col-xl-3 col-sm-6 mb-3">
@@ -13,10 +13,10 @@
                 <div class="card-body-icon">
                     <i class="fas fa-users"></i>
                 </div>
-                <div class="mr-5">{{ $users_count }} Пользователей</div>
+                <div class="mr-5">{{ $users_count }} {{ __('main/home.users') }}</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="#">
-                <span class="float-left">Подробнее</span>
+                <span class="float-left">{{ __('main/home.more') }}</span>
                 <span class="float-right">
               <i class="fas fa-angle-right"></i>
             </span>
@@ -29,10 +29,10 @@
                 <div class="card-body-icon">
                     <i class="fas fa-fw fa-book"></i>
                 </div>
-                <div class="mr-5">{{ $articles_count }} статей</div>
+                <div class="mr-5">{{ $articles_count }} {{ __('main/home.articles') }}</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="{{ route('knowledge') }}">
-                <span class="float-left">Перейти в базу знаний</span>
+                <span class="float-left">{{ __('main/home.to_knowledge_base') }}</span>
                 <span class="float-right">
               <i class="fas fa-angle-right"></i>
             </span>
@@ -45,10 +45,10 @@
                 <div class="card-body-icon">
                     <i class="fas fa-headset"></i>
                 </div>
-                <div class="mr-5">{{ $requests_count }} заявки(ок) в службу технической поддержки</div>
+                <div class="mr-5">{{ $requests_count }} {{ __('main/home.applications_to_support') }}</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="{{ route('requestsCreate') }}">
-                <span class="float-left">Обратиться в поддержку</span>
+                <span class="float-left">{{ __('main/home.contact_support') }}</span>
                 <span class="float-right">
               <i class="fas fa-angle-right"></i>
             </span>
@@ -61,10 +61,10 @@
                 <div class="card-body-icon">
                     <i class="fas fa-fw fa-calendar-alt"></i>
                 </div>
-                <div class="mr-5">{{ $events_count }} предстоящих событий в этом месяце</div>
+                <div class="mr-5">{{ $events_count }} {{ __('main/home.upcoming_month_events') }}</div>
             </div>
             <a class="card-footer text-white clearfix small z-1" href="{{ route('events.index') }}">
-                <span class="float-left">Подробнее</span>
+                <span class="float-left">{{ __('main/home.more') }}</span>
                 <span class="float-right">
               <i class="fas fa-angle-right"></i>
             </span>
@@ -80,46 +80,46 @@
         <div class="card mb-3">
             <div class="card-header">
                 <i class="fas fa-chart-area"></i>
-                Аналитическое сравнение заявок пользователей</div>
+                {{ __('main/home.user_requests_comparison') }}</div>
             <div class="card-body">
                 <canvas id="myAreaChart" width="100%" height="30"></canvas>
             </div>
-            <div class="card-footer small text-muted">Обновлено {{ date('Y-m-d H:i:s') }}</div>
+            <div class="card-footer small text-muted">{{ __('main/home.updated') }} {{ date('Y-m-d H:i:s') }}</div>
         </div>
     </div>
     <div class="col-md-3">
         <div class="card mb-3">
             <div class="card-header">
                 <i class="fas fa-chart-pie"></i>
-                Процентное соотношение заявок</div>
+                {{ __('main/home.applications_percentage') }}</div>
             <div class="card-body">
                 <canvas id="myPieChart" width="100%" height="100"></canvas>
             </div>
-            <div class="card-footer small text-muted">Обновлено {{ date('Y-m-d H:i:s') }}</div>
+            <div class="card-footer small text-muted">{{ __('main/home.updated') }} {{ date('Y-m-d H:i:s') }}</div>
         </div>
     </div>
     <div class="col-md-6">
         <div class="card mb-3">
-            <h5 class="card-header">Новости</h5>
+            <h5 class="card-header">{{ __('main/home.news') }}</h5>
 
             <ul class="list-unstyled mt-1 ml-1">
 
                 @forelse($news as $item)
                     <li class="media my-2">
                         @if(@getimagesize(asset('/storage/' . $item['image'])))
-                            <img src="{{ asset('/storage/' . $item['image']) }}" width="100px" height="100px" style="object-fit: cover;" class="mr-3 rounded" alt="Изображение">
+                            <img src="{{ asset('/storage/' . $item['image']) }}" width="100px" height="100px" style="object-fit: cover;" class="mr-3 rounded" alt="{{ __('main/home.image') }}">
                         @else
-                            <img src="/img/no_image.png" width="100px" height="100px" style="object-fit: cover;" class="mr-3 rounded" alt="Изображение">
+                            <img src="/img/no_image.png" width="100px" height="100px" style="object-fit: cover;" class="mr-3 rounded" alt="{{ __('main/home.image') }}">
                         @endif
                         <div class="media-body">
                             <h5 class="mt-0 mb-1">{{ $item['title'] }}</h5>
                             {{ mb_strimwidth(strip_tags(str_replace('&nbsp;', ' ', $item['text'])), 0, 350, '...') }}
-                            <a href="/news/post/{{ $item['id'] }}"><small>Подробнее</small></a>
+                            <a href="/news/post/{{ $item['id'] }}"><small>{{ __('main/home.more') }}</small></a>
                         </div>
                     </li>
                 @empty
                     <div class="alert alert-info" role="alert">
-                        <p>Новости в данной категории отсутсвуют</p>
+                        <p>{{ __('main/home.no_news') }}</p>
                     </div>
                 @endforelse
 
@@ -129,22 +129,22 @@
     </div>
     <div class="col-md-6">
         <div class="card">
-            <h5 class="card-header">Статьи</h5>
+            <h5 class="card-header">{{ __('main/home.articles') }}</h5>
 
             <ul class="list-unstyled mt-1 ml-1">
 
                 @forelse($articles as $item)
                     <li class="media my-2">
-                        <img src="/img/no_image_article5.svg" width="100px" height="100px" class="mr-3 rounded" alt="Изображение">
+                        <img src="/img/no_image_article5.svg" width="100px" height="100px" class="mr-3 rounded" alt="{{ __('main/home.image') }}">
                         <div class="media-body">
                             <h5 class="mt-0 mb-1">{{ $item['title'] }}</h5>
                             {{ mb_strimwidth(strip_tags(str_replace('&nbsp;', ' ', $item['text'])), 0, 350, '...') }}
-                            <a href="{{ route('knowledgeShow', $item['id']) }}"><small>Подробнее</small></a>
+                            <a href="{{ route('knowledgeShow', $item['id']) }}"><small>{{ __('main/home.more') }}</small></a>
                         </div>
                     </li>
                 @empty
                     <div class="alert alert-info" role="alert">
-                        <p>Статьи в данной категории отсутсвуют</p>
+                        <p>{{ __('main/home.no_articles') }}</p>
                     </div>
                 @endforelse
 
