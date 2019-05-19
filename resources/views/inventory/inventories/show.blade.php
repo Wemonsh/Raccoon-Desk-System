83efaa91b6d2cdfa9a720b28962408300f1e2cbc
Alexander Dyachenko <36398943+AlexNewmang@users.noreply.github.com>
2019-05-19 22:55:27 +0300

    MovementController - show, views\knowledge\show
    MovementController - show - правки верстки и ajax-table.
    views\knowledge\show - изменено расположение блоков.

--- resources/views/inventory/inventories/show.blade.php
+++ resources/views/inventory/inventories/show.blade.php
@@ -16,14 +16,11 @@
     <hr>
 
     <div class="row">
-        <div class="col-4">
-            <img src="{{ asset('/storage/' . $object->device->photo) }}" class="img-fluid img-thumbnail" alt="">
-
-
-
+        <div class="col-12 col-md-12 col-sm-12 col-lg-4 col-xl-4 mb-3">
+            <img src="{{ asset('/storage/' . $object->device->photo) }}" class="img-fluid img-thumbnail" alt="Изображение отсутствует">
         </div>
-        <div class="col-4">
-            <div class="card mt-3">
+        <div class="col-12 col-md-12 col-sm-12 col-lg-4 col-xl-4 mb-3">
+            <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Характеристики:</h5>
                     <table class="table table-sm">
@@ -39,7 +36,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-4">
+        <div class="col-12 col-md-12 col-sm-12 col-lg-4 col-xl-4">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Основная информация:</h5>
@@ -55,7 +52,7 @@
         </div>
 
         <div class="col-12">
-            <div class="card">
+            <div class="card mt-3 mb-3">
                 <div class="card-body">
                     <h5 class="card-title">История перемещений:</h5>
                     <div class="toolbar">
@@ -78,16 +75,16 @@
                         <thead>
 
                         <tr>
-                            <th data-field="created_at" rowspan="2">Дата</th>
+                            <th data-sortable="true" data-field="created_at" rowspan="2">Дата</th>
                             <th rowspan="1" colspan="2">Откуда</th>
                             <th rowspan="1" colspan="2">Куда</th>
                             <th data-field="comment" rowspan="2">Комментарий</th>
                         </tr>
                         <tr>
-                            <th data-field="id_placement_from">Помещение</th>
-                            <th data-field="id_responsible_from">Сотрудник</th>
-                            <th data-field="id_placement_to">Помещение</th>
-                            <th data-field="id_responsible_to">Сотрудник</th>
+                            <th data-sortable="true" data-field="id_placement_from" data-formatter="placementFromFormatter">Помещение</th>
+                            <th data-field="id_responsible_from" data-formatter="responsibleFromFormatter">Сотрудник</th>
+                            <th data-sortable="true" data-field="id_placement_to" data-formatter="placementToFormatter">Помещение</th>
+                            <th data-field="id_responsible_to" data-formatter="responsibleToFormatter">Сотрудник</th>
                         </tr>
                         </thead>
                     </table>
@@ -110,26 +107,37 @@
                 });
             }
 
-//            function organizationFormatter(value ,rows) {
-//                if (rows != null) {
-//                    return rows.organization.name;
-//                }
-//            }
+            function placementFromFormatter(value ,rows) {
+               if (rows != null) {
+                   return rows.placement_from.name;
+               }
+            }
 
-//            function actionFormatter(value ,rows) {
-//                return '<div class="btn-group" role="group" aria-label="Basic example">' +
-//                    '<a class="btn btn-secondary btn-sm text-light" href="/inventory/placements/edit/'+ rows['id'] +'" title="Редактировать"><i class="fas fa-pen"></i></a>' +
-//                    '<a class="btn btn-secondary btn-sm text-light" href="" title="Удалить"><i class="fas fa-trash-alt"></i></a>' +
-//                    '</div>';
-//            }
+            function responsibleFromFormatter(value ,rows) {
+                if (rows != null) {
+                    return rows.responsible_from.last_name + ' ' + rows.responsible_from.first_name + ' ' + rows.responsible_from.middle_name;
+                }
+            }
+
+            function placementToFormatter(value ,rows) {
+                if (rows != null) {
+                    return rows.placement_to.name;
+                }
+            }
+
+            function responsibleToFormatter(value ,rows) {
+                if (rows != null) {
+                    return rows.responsible_to.last_name + ' ' + rows.responsible_to.first_name + ' ' + rows.responsible_to.middle_name;
+                }
+            }
         </script>
 
         <div class="modal fade" id="movementModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
+            <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     {!! Form::open(array('route' => 'movementMove', 'method' => 'POST')) !!}
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Перемещение МТС</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -173,7 +181,7 @@
 
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
                         {!! Form::submit('Добавить', ['class' => 'btn btn-primary']) !!}
                     </div>
 
@@ -181,7 +189,7 @@
                 </div>
             </div>
         </div>
-
+    </div>
 
         {{--<div class="col-12">--}}
             {{--<div class="card">--}}
--- resources/views/knowledge/show.blade.php
+++ resources/views/knowledge/show.blade.php
@@ -12,34 +12,43 @@
 @endsection
 
 @section('content')
-<div class="card">
-    <div class="card-body">
-        <h1>{{ $article['title'] }}</h1>
-        <hr>
-        <div class="content">
-            {!! $article['text'] !!}
+    <div class="row">
+        <div class="col-12 col-md-12 col-sm-12 col-lg-12 col-xl-9">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h1>{{ $article['title'] }}</h1>
+                    <hr>
+                    <div class="content">
+                        {!! $article['text'] !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-12 col-sm-12 col-lg-12 col-xl-3">
+            <div class="card">
+                <div class="card-header">{{ __('knowledge/show.info') }}</div>
+                <div class="card-body">
+                    <p>{{ __('knowledge/show.date_create') }} {{ $article['created_at'] }}</p>
+                    <p>{{ __('knowledge/show.date_edit') }} {{ $article['updated_at'] }}</p>
+                    <p>{{ __('knowledge/show.views_number') }} {{ $article['views'] }}</p>
+                    <p>{{ __('knowledge/show.author') }} <a href="#">{{ $article['user']['last_name'].' '.$article['user']['first_name'].' '.$article['user']['middle_name'] }}</a></p>
+                </div>
+            </div>
+
+            @if($article['files'] != null)
+                <div class="card mt-3">
+                    <div class="card-header">{{ __('knowledge/show.attachments') }}</div>
+                    <div class="card-body">
+                        @foreach(json_decode($article['files']) as $file)
+                            <p><a href="{{ asset('/storage/' . $file->path) }}">{{ $file->name }}</a></p>
+                        @endforeach
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
-</div>
 
-<div class="card mt-3">
-    <div class="card-header">{{ __('knowledge/show.info') }}</div>
-    <div class="card-body">
-        <p>{{ __('knowledge/show.date_create') }} {{ $article['created_at'] }}</p>
-        <p>{{ __('knowledge/show.date_edit') }} {{ $article['updated_at'] }}</p>
-        <p>{{ __('knowledge/show.views_number') }} {{ $article['views'] }}</p>
-        <p>{{ __('knowledge/show.author') }} <a href="#">{{ $article['user']['last_name'].' '.$article['user']['first_name'].' '.$article['user']['middle_name'] }}</a></p>
-    </div>
-</div>
 
-@if($article['files'] != null)
-    <div class="card mt-3">
-        <div class="card-header">{{ __('knowledge/show.attachments') }}</div>
-        <div class="card-body">
-            @foreach(json_decode($article['files']) as $file)
-                <p><a href="{{ asset('/storage/' . $file->path) }}">{{ $file->name }}</a></p>
-            @endforeach
-        </div>
-    </div>
-@endif
+
 @endsection
